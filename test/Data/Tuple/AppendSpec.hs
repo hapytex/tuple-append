4b2d5d64c5601b5845c73e48a473792d4c0e18e4
Willem Van Onsem <vanonsem.willem@gmail.com>
2022-06-30 17:43:31 +0200

    first testcases

--- test/Data/Tuple/AppendSpec.hs
+++ test/Data/Tuple/AppendSpec.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE StandaloneDeriving #-}
+
 module Data.Tuple.AppendSpec where
 
 import Data.Tuple.Append(TupleAddL((<++)), TupleAddR((++>)), TupleAppend((+++)))
+import GHC.Tuple(Solo(Solo))
 
 import Test.Hspec
 
+deriving instance Eq a => Eq (Solo a)
+
+toAdd :: Int
+toAdd = 1
+
 spec :: Spec
 spec = do
-  describe "besselDenominators" $ fail "foo"
+  describe "TupleAddL" $ do
+    it "0-tuple" (toAdd <++ () `shouldBe` Solo 1)
+    it "1-tuple" (toAdd <++ Solo 4 `shouldBe` (1, 4))
+    it "2-tuple" (toAdd <++ (4, 2) `shouldBe` (1, 4, 2))
+    it "3-tuple" (toAdd <++ (4, 2, 5) `shouldBe` (1, 4, 2, 5))
