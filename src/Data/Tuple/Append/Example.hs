61370a188be9b04a14d71bf1764747c91bfae3c5
Willem Van Onsem <vanonsem.willem@gmail.com>
2022-07-01 18:45:06 +0200

    include imports in the #if

--- src/Data/Tuple/Append/Example.hs
+++ src/Data/Tuple/Append/Example.hs
@@ -3,13 +3,13 @@
 
 module Data.Tuple.Append.Example where
 
+#if MIN_VERSION_ghc_prim(0,7,0)
 import Data.Tuple.Append.TemplateHaskell(makeBoxedTupleAppendFun, makeUnboxedTupleAppendFun)
 
 import GHC.Exts(Float#, Int#)
 
 import Language.Haskell.TH.Syntax(Type(ConT, VarT), mkName)
 
-#if MIN_VERSION_ghc_prim(0,7,0)
 makeBoxedTupleAppendFun (mkName "append_if_f") [ ConT ''Int, ConT ''Float ] [ ConT ''Float ]
 makeUnboxedTupleAppendFun (mkName "uappend_ix_f") [ ConT ''Int#, VarT (mkName "a")] [ConT ''Float# ]
 #endif
