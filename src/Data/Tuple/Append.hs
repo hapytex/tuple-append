09126d95b25fc8dcf98b5b023d9e5ffde187ffe8
Willem Van Onsem <vanonsem.willem@gmail.com>
2022-11-26 13:37:09 +0100

    code formatting

--- src/Data/Tuple/Append.hs
+++ src/Data/Tuple/Append.hs
@@ -1,28 +1,35 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, QuasiQuotes, Safe #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE Safe #-}
 {-# OPTIONS_GHC -Wall -fno-warn-orphans -Werror #-}
 
-{-|
-Module      : Data.Tuple.Append
-Description : A module that contains typeclasses to prepend and append items and tuples into new tuples together with the corresponding instances.
-Maintainer  : hapytexeu+gh@gmail.com
-Stability   : experimental
-Portability : POSIX
+-- |
+-- Module      : Data.Tuple.Append
+-- Description : A module that contains typeclasses to prepend and append items and tuples into new tuples together with the corresponding instances.
+-- Maintainer  : hapytexeu+gh@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
+--
+-- A module that contains typeclasses to prepend and append items and tuples into new tuples together with the corresponding instances.
+module Data.Tuple.Append
+  ( -- * Add an element to a tuple
+    TupleAddL ((<++)),
+    TupleAddR ((++>)),
 
-A module that contains typeclasses to prepend and append items and tuples into new tuples together with the corresponding instances.
--}
-
-module Data.Tuple.Append(
-    -- * Add an element to a tuple
-    TupleAddL((<++)), TupleAddR((++>))
     -- * Append two tuples
-  , TupleAppend((+++))
-    -- * Sequence a tuple
-  , SequenceTuple(sequenceTupleA, sequenceTupleA_)
-  ) where
+    TupleAppend ((+++)),
 
-import Data.Tuple.Append.Class(TupleAddL((<++)), TupleAddR((++>)), TupleAppend((+++)), SequenceTuple(sequenceTupleA, sequenceTupleA_))
-import Data.Tuple.Append.TemplateHaskell(defineTupleAddUpto, defineTupleAppendUpto, defineSequenceTupleUpTo)
+    -- * Sequence a tuple
+    SequenceTuple (sequenceTupleA, sequenceTupleA_),
+  )
+where
+
+import Data.Tuple.Append.Class (SequenceTuple (sequenceTupleA, sequenceTupleA_), TupleAddL ((<++)), TupleAddR ((++>)), TupleAppend ((+++)))
+import Data.Tuple.Append.TemplateHaskell (defineSequenceTupleUpTo, defineTupleAddUpto, defineTupleAppendUpto)
 
 [defineTupleAddUpto|61|]
+
 [defineTupleAppendUpto|19|]
+
 [defineSequenceTupleUpTo|62|]
