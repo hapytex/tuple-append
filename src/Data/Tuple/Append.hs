df4e15eb288b12ae2d791fff368644497a73a609
Willem Van Onsem <vanonsem.willem@gmail.com>
2023-05-21 20:55:31 +0200

    ormolu

--- src/Data/Tuple/Append.hs
+++ src/Data/Tuple/Append.hs
@@ -4,17 +4,16 @@
 {-# LANGUAGE Safe #-}
 {-# OPTIONS_GHC -Wall -fno-warn-orphans -Werror #-}
 
-{- |
- Module      : Data.Tuple.Append
- Description : A module that contains typeclasses to prepend and append items and tuples into new tuples together with the corresponding instances.
- Maintainer  : hapytexeu+gh@gmail.com
- Stability   : experimental
- Portability : POSIX
-
- A module that contains typeclasses to prepend and append items and tuples into new tuples together with the corresponding instances.
--}
-module Data.Tuple.Append (
-    -- * Add an element to a tuple
+-- |
+-- Module      : Data.Tuple.Append
+-- Description : A module that contains typeclasses to prepend and append items and tuples into new tuples together with the corresponding instances.
+-- Maintainer  : hapytexeu+gh@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
+--
+-- A module that contains typeclasses to prepend and append items and tuples into new tuples together with the corresponding instances.
+module Data.Tuple.Append
+  ( -- * Add an element to a tuple
     TupleAddL ((<++)),
     TupleAddR ((++>)),
 
@@ -26,7 +25,8 @@ module Data.Tuple.Append (
 
     -- * Folding a tuple
     FoldTuple (foldlTuple, foldrTuple, foldMapTuple),
-) where
+  )
+where
 
 import Data.Tuple.Append.Class (FoldTuple (foldMapTuple, foldlTuple, foldrTuple), SequenceTuple (sequenceTupleA, sequenceTupleA_), TupleAddL ((<++)), TupleAddR ((++>)), TupleAppend ((+++)))
 import Data.Tuple.Append.TemplateHaskell (defineFoldTupleUpTo, defineSequenceTupleUpTo, defineTupleAddUpto, defineTupleAppendUpto)
